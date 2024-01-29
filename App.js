1f73d5ff67a399ce099b8e142fbefea80a6fa554
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2024-01-29 16:44:45 +0200

    ДЗ 36

--- App.js
+++ App.js
@@ -1,10 +1,71 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var imageNames = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
+//получение поста
+function fetchPost(postId) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => resolve(data))
+            .catch(error => reject(error));
+    });
+}
 
-    var randomIndex = Math.floor(Math.random() * imageNames.length);
+//получения комента
+function fetchComments(postId) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => resolve(data))
+            .catch(error => reject(error));
+    });
+}
 
-    var randomImagePath = "images/" + imageNames[randomIndex];
+// отображение про пост
+function displayPost(post) {
+    const postContainer = document.getElementById('post-container');
+    postContainer.innerHTML = `
+        <h2>Post Information</h2>
+        <p><strong>Title:</strong> ${post.title}</p>
+        <p><strong>Body:</strong> ${post.body}</p>
+        <button onclick="fetchAndDisplayComments(${post.id})">Get Comments</button>
+    `;
+}
 
-    var imgElement = document.getElementById("randomImage");
-    imgElement.src = randomImagePath;
-});
+//отображение коментов
+function displayComments(comments) {
+    const postContainer = document.getElementById('post-container');
+    const commentsList = document.createElement('ul');
+
+    comments.forEach(comment => {
+        const commentItem = document.createElement('li');
+        commentItem.textContent = `${comment.name}: ${comment.body}`;
+        commentsList.appendChild(commentItem);
+    });
+
+    postContainer.appendChild(commentsList);
+}
+
+//тоже отображение
+function fetchAndDisplayComments(postId) {
+    fetchComments(postId)
+        .then(comments => displayComments(comments))
+        .catch(error => console.error(error));
+}
+
+//поиск поста
+function searchPost() {
+    const postIdInput = document.getElementById('postId');
+    const postId = postIdInput.value;
+
+    fetchPost(postId)
+        .then(post => displayPost(post))
+        .catch(error => console.error(error));
+}
