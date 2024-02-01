f32e81d71cea4ca0042fd603eac80e3c95c275a5
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2024-02-01 20:30:55 +0200

    ДЗ 38

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var ukraineCities = [
-    'Kyiv', 'Kharkiv', 'Odesa', 'Dnipro', 'Lviv', 'Zaporizhia', 'Kryvyi Rih', 'Mykolaiv', 'Mariupol', 'Luhansk'
-];
+document.addEventListener('DOMContentLoaded', function () {
+    getCities();
+
+    document.getElementById('cities').addEventListener('change', function () {
+        getWeather();
+    });
+});
+
+var ukraineCities = ['Kyiv', 'Kharkiv', 'Odesa', 'Dnipro', 'Lviv', 'Zaporizhia', 'Kryvyi Rih', 'Mykolaiv', 'Mariupol', 'Luhansk'];
 
 function getCities() {
     var citiesSelect = document.getElementById('cities');
-    ukraineCities.forEach(function(city) {
+    ukraineCities.forEach(function (city) {
         var option = document.createElement('option');
         option.value = city;
         option.text = city;
@@ -34,21 +40,19 @@ function getWeather() {
 
             document.getElementById('weather-info').innerHTML =
                 '<div class="card">' +
-                    '<div class="card-body">' +
-                        '<h5 class="card-title">Погода в місті ' + selectedCity + '</h5>' +
-                        '<p class="card-text">Температура: ' + temperature + '°C</p>' +
-                        '<p class="card-text">Тиск: ' + pressure + ' hPa</p>' +
-                        '<p class="card-text">Опис: ' + description + '</p>' +
-                        '<p class="card-text">Вологість: ' + humidity + '%</p>' +
-                        '<p class="card-text">Швидкість вітру: ' + windSpeed + ' м/с</p>' +
-                        '<p class="card-text">Напрям вітру: ' + windDirection + ' градусів</p>' +
-                        '<img src="http://openweathermap.org/img/w/' + iconCode + '.png" class="img-fluid" alt="Погодний значок">' +
-                    '</div>' +
+                '<div class="card-body">' +
+                '<h5 class="card-title">Погода в місті ' + selectedCity + '</h5>' +
+                '<p class="card-text">Температура: ' + temperature + '°C</p>' +
+                '<p class="card-text">Тиск: ' + pressure + ' hPa</p>' +
+                '<p class="card-text">Опис: ' + description + '</p>' +
+                '<p class="card-text">Вологість: ' + humidity + '%</p>' +
+                '<p class="card-text">Швидкість вітру: ' + windSpeed + ' м/с</p>' +
+                '<p class="card-text">Напрям вітру: ' + windDirection + ' градусів</p>' +
+                '<img src="http://openweathermap.org/img/w/' + iconCode + '.png" class="img-fluid" alt="Погодний значок">' +
+                '</div>' +
                 '</div>';
         }
     };
 
     xhr.send();
 }
-
-window.onload = getCities;
