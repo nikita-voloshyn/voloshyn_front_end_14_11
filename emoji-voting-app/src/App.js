d6f6bbd4da8fa03b2b7f51bbd6968117471f39f5
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2024-03-26 19:30:35 +0200

    dz-46

---
+++ emoji-voting-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import EmojiVoting from './components/EmojiVoting';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <EmojiVoting />
+      </div>
+    );
+  }
+}
+
+export default App;
